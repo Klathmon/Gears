6d39e6bf3c244c1b9f194e53dc29f0c808510090
Gregory Benner <gregbenner1@gmail.com>
2014-01-03 09:42:25 -0500

    Created Router class and tests

---
+++ Gears/Router/Router.php
@@ -0,0 +1,101 @@
+<?php
+namespace Gears\Router;
+
+use Gears\Exceptions\FileNotReadableException;
+use Gears\Exceptions\InvalidFileFormatException;
+use InvalidArgumentException;
+
+/*
+ * TODO: Full DocBlock annotations
+ * TODO: Reverse route matching
+ * TODO: Calling errors manually
+ * TODO: Base route modification
+ */
+
+class Router
+{
+    const ROUTE_SEPARATOR = '/';
+    const NAMED_PARAMETER_PREFIX = ':';
+    protected $routesFileArray;
+
+
+    public function __construct($routesFile)
+    {
+        if (!is_string($routesFile) || $routesFile == '') {
+            throw new InvalidArgumentException('Parameter $routesFile must be a valid string!');
+        } elseif (!is_readable($routesFile) || ($fileContents = file_get_contents($routesFile)) === false) {
+            throw new FileNotReadableException('Routes JSON file is not readable!');
+        } elseif (is_null($routes = json_decode($fileContents, true))) {
+            throw new InvalidFileFormatException('Routes JSON file cannot be parsed!');
+        } elseif (!isset($routes['errors']['404'])) {
+            throw new InvalidFileFormatException('Routes JSON file must have 404 error route!');
+        } else {
+            $this->routesFileArray = $routes;
+        }
+    }
+
+    public function parseRoute($request)
+    {
+        $requestParts = self::partOutRoute($request);
+
+        foreach ($this->routesFileArray['routes'] as $name => $route) {
+            $routeParts = self::partOutRoute($route['route']);
+
+            if (($namedParameters = $this->routeMatchesRequest($routeParts, $requestParts)) !== false) {
+                return array_merge($this->getRouteInfo($route, $namedParameters), ['name' => $name]);
+            }
+        }
+
+        //No routes matched... Return the 404 error stuff...
+        return array_merge($this->getRouteInfo($this->routesFileArray['errors']['404']), ['name' => '404']);
+    }
+
+    private function routeMatchesRequest($routeParts, $requestParts)
+    {
+        if (count($routeParts) != count($requestParts)) {
+            return false; //Return false if the number of parts don't match.
+        }
+
+        $namedParameters = [];
+        foreach ($requestParts as $partNumber => $requestPart) {
+            $routePart = $routeParts[$partNumber];
+
+            if ($routePart[0] == self::NAMED_PARAMETER_PREFIX) {
+                $namedParameters[$routePart] = $requestPart;
+            } elseif ($routePart != $requestPart) {
+                return false; //At the first non-matching thing, return false.
+            }
+        }
+
+        //If we are here, that means the route matches! return the named parameters.
+        return $namedParameters;
+    }
+
+    private function getRouteInfo($route, $namedParameters = [])
+    {
+        //Route matches the request, get the values.
+        $returnVal['controller'] = $this->getValue($route['controller'], $namedParameters);
+        $returnVal['action']     = $this->getValue($route['action'], $namedParameters);
+        $returnVal['parameters'] = [];
+        foreach ($route['parameters'] as $parameter) {
+            $key                           = ($parameter[0] == self::NAMED_PARAMETER_PREFIX ? substr($parameter, 1) : $parameter);
+            $returnVal['parameters'][$key] = $this->getValue($parameter, $namedParameters);
+        }
+
+        return $returnVal;
+    }
+
+    private function getValue($command, $namedParameters)
+    {
+        if ($command[0] == self::NAMED_PARAMETER_PREFIX && array_key_exists($command, $namedParameters)) {
+            return $namedParameters[$command];
+        } else {
+            return $command;
+        }
+    }
+
+    private static function partOutRoute($route)
+    {
+        return explode(self::ROUTE_SEPARATOR, trim($route, self::ROUTE_SEPARATOR));
+    }
+}
---
+++ Tests/Router/RouterTest.php
@@ -0,0 +1,66 @@
+<?php
+namespace Gears\Router;
+
+use InvalidArgumentException;
+
+class RouterTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testExceptionOnBadConstructor()
+    {
+        /** @noinspection PhpParamsInspection */
+        $router = new Router(5);
+
+        $this->assertNotInstanceOf('\\Gears\\Router\\Router', $router);
+    }
+
+    /**
+     * @expectedException \Gears\Exceptions\FileNotReadableException
+     */
+    public function testExceptionOnRoutesFileNotFound()
+    {
+        $router = new Router('notARealFile.json');
+
+        $this->assertNotInstanceOf('\\Gears\\Router\\Router', $router);
+    }
+
+    /**
+     * @expectedException \Gears\Exceptions\FileNotReadableException
+     */
+    public static function testExceptionOnIncorrectJsonFormat()
+    {
+        $router = new Router('testBadRoutes.json');
+
+        self::assertNotInstanceOf('\\Gears\\Router\\Router', $router);
+    }
+    
+    /**
+     * @dataProvider providerTestParseRoute
+     */
+    public function testParseRoute($route, $expectedName, $expectedController, $expectedAction, $expectedParameters)
+    {
+        $router = new Router(__DIR__ . DIRECTORY_SEPARATOR . 'testRoutes.json');
+
+        $this->assertInstanceOf('\\Gears\\Router\\Router', $router);
+
+        $parsedRoute = $router->parseRoute($route);
+
+        $this->assertEquals($expectedName, $parsedRoute['name']);
+        $this->assertEquals($expectedController, $parsedRoute['controller']);
+        $this->assertEquals($expectedAction, $parsedRoute['action']);
+        $this->assertEquals($expectedParameters, $parsedRoute['parameters']);
+    }
+
+    public static function providerTestParseRoute()
+    {
+        return [
+            ['/testroute1', 'test1', 'Test1', 'show', ['param1value' => 'param1value', 'param2value' => 'param2value']],
+            ['/testroute2/123/456', 'test2', 'Test2', 'show', ['value1' => '123', 'value2' => '456']],
+            ['/testroute3/MyController/myAction/myParameter', 'test3', 'MyController', 'myAction', ['param1' => 'myParameter']],
+            ['/not/a/real/route', '404', 'ErrorController', 'fourOhFour', []],
+        ];
+    }
+}
+ 
