649bc1025047984f71a53b6c6afeac9a0e92a352
Gregory Benner <gregbenner1@gmail.com>
2014-01-03 18:18:29 -0500

    Added DependencyInjector

---
+++ Tests/DependencyInjectorTest.php
@@ -0,0 +1,87 @@
+<?php
+namespace Gears;
+
+use DateTime;
+
+class DependencyInjectorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var DependencyInjector
+     */
+    protected $di;
+
+    protected function setup()
+    {
+        $this->di = new DependencyInjector();
+
+        self::assertInstanceOf('\\Gears\\DependencyInjector', $this->di);
+    }
+
+    protected function tearDown()
+    {
+        unset($this->di);
+    }
+
+    public function testAddingDependency()
+    {
+        /** @noinspection PhpUnusedParameterInspection */
+        $this->di->addDependency('testDateTime', function($di){
+                return new DateTime('now');
+            });
+    }
+    
+    public function testGettingDependencyNewInstance()
+    {
+        $this->testAddingDependency();
+        
+        $dateTime = $this->di->getDependency('testDateTime', true);
+        
+        $this->assertInstanceOf('\\DateTime', $dateTime);
+    }
+    
+    public function testGettingDependencySameInstance()
+    {
+        $this->testAddingDependency();
+        
+        $dateTime = $this->di->getDependency('testDateTime', false);
+        
+        $this->assertInstanceOf('\\DateTime', $dateTime);
+    }
+    
+    public function testGettingDependencySameInstanceMoreThanOnce()
+    {
+        $this->testAddingDependency();
+
+        /** @var DateTime $dateTime */
+        $dateTime = $this->di->getDependency('testDateTime', false);
+        
+        $this->assertInstanceOf('\\DateTime', $dateTime);
+        
+        /** @var DateTime $dateTime2 */
+        $dateTime2 = $this->di->getDependency('testDateTime', false);
+        
+        $this->assertInstanceOf('\\DateTime', $dateTime2);
+        
+        //They both refer to the same object
+        $this->assertTrue(($dateTime === $dateTime2));
+    }
+    
+    public function testGettingDependencyNewInstanceMoreThanOnce()
+    {
+        $this->testAddingDependency();
+
+        /** @var DateTime $dateTime */
+        $dateTime = $this->di->getDependency('testDateTime', true);
+        
+        $this->assertInstanceOf('\\DateTime', $dateTime);
+        
+        /** @var DateTime $dateTime2 */
+        $dateTime2 = $this->di->getDependency('testDateTime', true);
+        
+        $this->assertInstanceOf('\\DateTime', $dateTime2);
+        
+        //They both don't refer to the same object
+        $this->assertTrue(($dateTime !== $dateTime2));
+    }
+}
+ 
